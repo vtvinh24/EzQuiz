c71aa1563998261694f9b16fe905693afb151501
Nguyen Huy Tam <tamnhhe@gmail.com>
2025-07-05 19:14:39 +0700

    Update Splash screen and Logo

---
+++ app/src/main/java/dev/vtvinh24/ezquiz/ui/SplashActivity.java
@@ -0,0 +1,28 @@
+package dev.vtvinh24.ezquiz.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.vtvinh24.ezquiz.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
+
